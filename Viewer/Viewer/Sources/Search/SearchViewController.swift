79365b17dabcd257bce827afe9b75afe77f81044
Iustin Bulimar <iustin.bulimar@sparktech.ro>
2018-09-17 20:41:25 +0300

    Fix bug

--- Viewer/Viewer/Sources/Search/SearchViewController.swift
+++ Viewer/Viewer/Sources/Search/SearchViewController.swift
@@ -10,6 +10,9 @@ class SearchViewController: UIViewController {
     var page = 1
     var pages = 1
     
+    var animationAreaFrame: CGRect?
+    var selectedIndexPath: IndexPath?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,7 +25,7 @@ class SearchViewController: UIViewController {
         searchBar.becomeFirstResponder()
     }
     
-    func getNextSearchPhotosPage() {
+    func getNextPhotosPage() {
         guard page <= pages else { return }
         RemoteData.getPhotos(type: .search(page: page, text: searchBar.text ?? ""))
             .done { photosResponse in
@@ -62,13 +65,15 @@ extension SearchViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let photo = photos[indexPath.row]
         let photoViewerViewController = PhotoViewerViewController.storyboardInstance(url: photo.url)
+        photoViewerViewController.transitioningDelegate = self
+        selectedIndexPath = indexPath
         present(photoViewerViewController, animated: true)
         tableView.deselectRow(at: indexPath, animated: false)
     }
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         if indexPath.row + 1 == photos.count {
-            getNextSearchPhotosPage()
+            getNextPhotosPage()
         }
     }
     
@@ -96,11 +101,63 @@ extension SearchViewController: UISearchBarDelegate {
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         view.endEditing(true)
-        getNextSearchPhotosPage()
+        page = 1
+        pages = 1
+        photos = []
+        getNextPhotosPage()
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         view.endEditing(true)
+    }
+    
+}
+
+
+extension SearchViewController: UIViewControllerTransitioningDelegate {
+    
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        
+        guard let selectedIndexPath = selectedIndexPath,
+            let cell = tableView.cellForRow(at: selectedIndexPath) as? PhotoCell,
+            let selectedImageView = cell.photoImageView,
+            let photoViewerViewController = presented as? PhotoViewerViewController,
+            let destinationImageView = photoViewerViewController.photoImageView else {
+                return nil
+        }
+        
+        animationAreaFrame = view.convert(tableView.frame, to: nil)
+        let fromImageViewRelativeFrame = selectedImageView.convert(selectedImageView.frame, to: nil)
+        
+        let toImageViewRelativeFrame = photoViewerViewController.view.frame.insetBy(dx: 0, dy: UIApplication.shared.statusBarFrame.height).offsetBy(dx: 0, dy: UIApplication.shared.statusBarFrame.height / 2)
+        
+        return ImagePreviewAnimator(fromImageView: selectedImageView,
+                                    toImageView: destinationImageView,
+                                    animationAreaFrame: animationAreaFrame!,
+                                    fromImageViewRelativeFrame: fromImageViewRelativeFrame,
+                                    toImageViewRelativeFrame: toImageViewRelativeFrame)
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        
+        guard let selectedIndexPath = selectedIndexPath,
+            let cell = tableView.cellForRow(at: selectedIndexPath) as? PhotoCell,
+            let selectedImageView = cell.photoImageView,
+            let photoViewerViewController = dismissed as? PhotoViewerViewController,
+            let sourceImageView = photoViewerViewController.photoImageView else {
+                return nil
+        }
+        
+        let fromImageViewRelativeFrame = sourceImageView.superview!.convert(sourceImageView.frame, to: nil)
+        let toImageViewRelativeFrame = selectedImageView.superview!.convert(selectedImageView.frame, to: self.view)
+        
+        self.selectedIndexPath = nil
+        
+        return ImagePreviewAnimator(fromImageView: sourceImageView,
+                                    toImageView: selectedImageView,
+                                    animationAreaFrame: animationAreaFrame!,
+                                    fromImageViewRelativeFrame: fromImageViewRelativeFrame,
+                                    toImageViewRelativeFrame: toImageViewRelativeFrame)
     }
     
 }
